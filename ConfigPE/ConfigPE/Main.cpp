8cc092b50aa9b0b8c3d1b706ad23007b309c2371
jrc3123 <jrc3123@rit.edu>
2020-08-22 16:26:30 -0400

    E01
    Configuring Visual Studio settings and setting up Z_DELETE and things like that

---
+++ ConfigPE/ConfigPE/Main.cpp
@@ -0,0 +1,11 @@
+#include "Main.h"
+
+int main()
+{
+	std::cout << "Hello World\n";
+
+	// ending the program
+	std::cout << "Press enter to finish";
+	getchar();
+	return 0;
+}
---
+++ ConfigPE/ConfigPE/Main.h
@@ -0,0 +1,7 @@
+// prevents the compiler from compiling this more than once
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <iostream>
+
+#endif // _MAIN_H
