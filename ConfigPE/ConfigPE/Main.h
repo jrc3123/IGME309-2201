1e76339d2bfc5206c369937a4994505ca3f7addd
jrc3123 <jrc3123@rit.edu>
2020-08-29 19:00:44 -0400

    E02
    Updating ConfigPE to reflect Exercise 2

--- ConfigPE/ConfigPE/Main.h
+++ ConfigPE/ConfigPE/Main.h
@@ -4,4 +4,12 @@
 
 #include <iostream>
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "GL\glew.h"
+#include "GLFW/glfw3.h"
+#include "GLM/glm.hpp"
+#include "OpenGL-Tutorials/shader.hpp"
+
 #endif // _MAIN_H
